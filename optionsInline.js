0caf6a79e360244d8d6a8e0dae4b6d95a07cbfb9
Denis Chmel <denis@goldenplanet.com>
2012-08-12 12:22:46 +0300

    Корректно переехал на manifest v2 http://developer.chrome.com/extensions/manifestVersion.html

---
+++ optionsInline.js
@@ -0,0 +1,7 @@
+/**
+ * Idiotic file, should be inline JS, but Chrome forbid inline JS in manifest v2
+ * http://developer.chrome.com/extensions/contentSecurityPolicy.html
+ */
+function onSettingsLoadedCallback() {
+    restoreOptions();
+}
